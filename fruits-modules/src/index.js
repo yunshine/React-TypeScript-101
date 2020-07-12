67267ef29f6b370b9484244bca02c4425d573779
Yun Chung <yun.chung@mac.com>
2020-07-12 15:49:22 +0900

    Completed Fruits Module challenge

--- fruits-modules/src/index.js
+++ fruits-modules/src/index.js
@@ -1,9 +1,22 @@
-function choice(items) {
-// selects random item from array
-}
+// import React, { Component } from "react";
+import foods from "./foods";
+import { choice, remove } from "./helpers";
 
-function remove(items, item) {
-// removes first matching item from items, if the item exists, and returns it. Otherwise returns undefined.
-}
 
-export { choice, remove };
+// Randomly draw a fruit from the array
+let randomChoice = choice(foods);
+
+// Log the message “I’d like one RANDOMFRUIT, please.”
+console.log(`I'd like one ${randomChoice}, please.`);
+
+// Log the message “Here you go: RANDOMFRUIT”
+console.log(`Here you go: ${randomChoice}`);
+
+// Log the message “Delicious! May I have another?”
+console.log("Delicious! May I have another?");
+
+// Remove the fruit from the array of fruits
+let remainingFood = remove(foods, randomChoice);
+
+// Log the message “I’m sorry, we’re all out. We have FRUITSLEFT left.”
+console.log(`I'm sorry, we're all out. But, we have ${remainingFood.length} other foods left`);
