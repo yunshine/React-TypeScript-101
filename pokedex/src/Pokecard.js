80c869ffbf4a409a569e6ca9dad459eeb8d6ed70
Yun Chung <yun.chung@mac.com>
2020-07-13 14:20:50 +0900

    Added a function to access an API for better photos of the Pokemon characters

--- pokedex/src/Pokecard.js
+++ pokedex/src/Pokecard.js
@@ -1,11 +1,15 @@
 import React, { Component } from 'react';
 import './Pokecard.css';
 
-const pokeURL = 'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/';
+// const pokeURL = 'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/';
+
+const pokeURL = 'https://assets.pokemon.com/assets/cms2/img/pokedex/detail/'
+
+let padToThree = (number) => (number <= 999 ? `00${number}`.slice(-3) : number);
 
 class Pokecard extends Component {
   render() {
-    let imgURL = `${pokeURL}${this.props.id}.png`
+    let imgURL = `${pokeURL}${padToThree(this.props.id)}.png`
     return (
       <div className="Pokecard">
         <h1 className="Pokecard-title">{this.props.name}</h1>
