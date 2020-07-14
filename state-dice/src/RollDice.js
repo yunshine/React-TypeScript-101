7fb4ce0ddbeaca7aa21ef36070cdd512963f0706
Yun Chung <yun.chung@mac.com>
2020-07-14 19:04:45 +0900

    Button text and disabled function changes on click

--- state-dice/src/RollDice.js
+++ state-dice/src/RollDice.js
@@ -8,7 +8,7 @@ class RollDice extends Component {
   };
   constructor(props) {
     super(props);
-    this.state = { die1: 'one', die2: 'one' };
+    this.state = { die1: 'one', die2: 'one', rolling: false };
     this.roll = this.roll.bind(this);
   }
 
@@ -16,7 +16,10 @@ class RollDice extends Component {
     const newDie1 = this.props.sides[Math.floor(Math.random() * this.props.sides.length)];
     const newDie2 = this.props.sides[Math.floor(Math.random() * this.props.sides.length)];
     // console.log(newDie1, newDie2);
-    this.setState({ die1: newDie1, die2: newDie2 });
+    this.setState({ die1: newDie1, die2: newDie2, rolling: true });
+    setTimeout(() => {
+      this.setState({ rolling: false });
+    }, 1000);
   }
 
   render() {
@@ -26,7 +29,9 @@ class RollDice extends Component {
         <Die face={this.state.die1} />
         <Die face={this.state.die2} />
       </div>
-        <button onClick={this.roll}>Roll Dice Now</button>
+        <button onClick={this.roll} disabled={this.state.rolling}>
+          {this.state.rolling ? "...rolling..." : "Roll Dice Now"}
+        </button>
       </div>
     );
   }
