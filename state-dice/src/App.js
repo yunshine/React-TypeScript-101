fd11cff7cc4d843594d5bcd8c78e3dbc60c1ac76
Yun Chung <yun.chung@mac.com>
2020-07-14 17:46:27 +0900

    Created Die components

--- state-dice/src/App.js
+++ state-dice/src/App.js
@@ -1,12 +1,17 @@
 import React from 'react';
 import './App.css';
 import RollDice from './RollDice';
+import Die from './Die';
 
 function App() {
   return (
     <div className="App">
-      <h1>Testing...</h1>
-      <RollDice />
+      {/* <h1>Testing...</h1>
+      <RollDice /> */}
+      <Die face="five" />
+      <Die face="three" />
+      <Die face="one" />
+      <Die face="two" />
     </div>
   );
 }
---
+++ state-dice/src/Die.js
@@ -0,0 +1,27 @@
+import React, { Component } from 'react'
+import './Die.css';
+
+class Die extends Component {
+  constructor(props) {
+    super(props);
+    this.state = { number: 1 };
+    // this.-something- = this.-something-.bind(this);
+  }
+
+  // -something-() {
+  //   this.setState({ key: value });
+  // }
+
+  render() {
+      return (
+      <div className="Die">
+        <i className={`fas fa-dice-${this.props.face}`}></i>
+      </div>
+    );
+  }
+}
+
+export default Die;
+
+      // <h1>{this.state.-something-}</h1>
+      // <h1>{this.props.-something-}</h1>
