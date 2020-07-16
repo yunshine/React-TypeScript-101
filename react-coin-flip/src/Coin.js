36e981965c1234617fb5706883dd127a321e46e3
Yun Chung <yun.chung@mac.com>
2020-07-16 12:27:58 +0900

    Completed coin component and coin frontend

--- react-coin-flip/src/Coin.js
+++ react-coin-flip/src/Coin.js
@@ -1,35 +1,14 @@
 import React, { Component } from 'react' // imrc is the shortcut...
-import './Coin.css'; // make a CSS file for this component...
+import './Coin.css';
 
 class Coin extends Component {
-  // static defaultProps = {
-  //   key: value,
-  // }
-  constructor(props) {
-    super(props);
-    // this.state = { key: value };
-    // this.Coin = this.Coin.bind(this);
-  }
-
-  // Coin() {
-  //   this.setState({ key: value });
-  // }
-
-  // tripleKill() {
-  //   this.setState(oldState => {
-  //     return { score: oldState + 3 };
-  //   })
-  // }   => This is the way and the syntax to update an existing state, not:   this.setState({ score: this.state.score + 3 });
-
   render() {
-      return (
+    return (
       <div className="Coin">
-      {/* // <h1>{this.state.Coin}</h1>
-      // <h1>{this.props.Coin}</h1> */}
-        <h1>This is the Coin component...</h1>
+        <img src='https://tinyurl.com/react-coin-tails-jpg' alt='One side of a coin' />
       </div>
     );
   }
 }
 
-export default Coin; /// connect this component to App.js...
+export default Coin;
