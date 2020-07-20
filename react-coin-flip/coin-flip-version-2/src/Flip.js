4b05e15d7645a3092c8ce375de81dcd8ef47ed4a
Yun Chung <yun.chung@mac.com>
2020-07-20 13:04:05 +0900

    Completed Coin Flip challenge

--- react-coin-flip/coin-flip-version-2/src/Flip.js
+++ react-coin-flip/coin-flip-version-2/src/Flip.js
@@ -5,7 +5,7 @@ import Coin from './Coin'
 
 class Flip extends Component {
   static defaultProps = {
-    coin: [{side: 'heads', imageSource: 'https://tinyurl.com/react-coin-heads-jpg'}, {side: 'tails', imageSource: 'https://tinyurl.com/react-coin-tails-jpg'}],
+    coin: [{side: 'heads', imageSource: 'https://tinyurl.com/react-coin-heads-jpg'}, {side: 'tails', imageSource: 'http://www.pcgscoinfacts.com/UserImages/71009269r.jpg?__cf_chl_jschl_tk__=210c4f91c38c039019258b6c88a394d619043c69-1595217790-0-AQKR1nT3c6RgMPfwZK1_WmTMy_IyJ1-EzRhQbkzRooJnk0YM7tS6NLEeNwoPxh48_fX5Pb030ixNcEimy3g1gsHSxxWndNgqPBSmeVpy7rlguRLRnOlbrgo6NWdJrbny9q8jmebAWKOa63_khfXoisR4kPeHz5Zk20nfoTqx08sf6BZdrnZl-JiTk3WznGGxDAUZhAnVpo4M0FlU1PZxKR6mTW5KP7qegl3n-hfVY-3iOLDV9iVEjIQOqwucKkkB04I5_VHv6xHwcpG2ozrDEIKFefW2zmIlA7DTgIfL86igsc-Umui9B1gbnRbMHj1--g'}],
   }
   constructor(props) {
     super(props);
@@ -20,30 +20,26 @@ class Flip extends Component {
     let randomIndex = Math.floor(Math.random() * array.length);
     if (randomIndex === 0) {
       this.setState(oldState => {
-        return { headsResults: oldState.headsResults + 1, currentSide: randomIndex, };
+        return { headsResults: oldState.headsResults + 1, currentSide:  this.props.coin[0], };
       })
     } else {
       this.setState(oldState => {
-        return { tailsResults: oldState.tailsResults + 1, currentSide: randomIndex };
+        return { tailsResults: oldState.tailsResults + 1, currentSide: this.props.coin[1] };
       })
     }
-
     console.log("a click has happened...", "here's my array: ", array, "here's the random index", randomIndex, "totalFlips: ", this.state.totalFlips, this. state.currentSide);
   }
 
   handleClick() {
     this.flipCoin(this.props.coin);
-  //   this.setState(oldState => {
-  //     return { score: oldState + 3 };
-  //   })
   }   
-  // => This is the way and the syntax to update an existing state, not:   this.setState({ score: this.state.score + 3 });
+
 
   render() {
       return (
       <div className="Flip">
         <h1>Flip a Coin!</h1>
-        <Coin coinInfo={this.props.coin[0]} />
+        <Coin coinInfo={this.state.currentSide} />
         <button onClick={this.handleClick} >Flip the Coin</button>
       <h1>Out of {this.state.totalFlips} flips, there have been {this.state.headsResults} heads and {this.state.tailsResults} tails.</h1>
       </div>
