3237ba174299ce43eaf0132e717c43fcd02e5ed5
Yun Chung <yun.chung@mac.com>
2020-07-21 15:07:02 +0900

    Able to display the board with random cells being lit

--- Lights Out/src/Board.js
+++ Lights Out/src/Board.js
@@ -30,10 +30,17 @@ import './Board.css';
  **/
 
 class Board extends Component {
-
+  static defaultProps = {
+    nrows: 5,
+    ncols: 5,
+    chanceLightStartsOn: 0.25,
+  };
   constructor(props) {
     super(props);
-
+    this.state = {
+      hasWon: false,
+      board: this.createBoard(),
+    }
     // TODO: set initial state
   }
 
@@ -41,7 +48,13 @@ class Board extends Component {
 
   createBoard() {
     let board = [];
-    // TODO: create array-of-arrays of true/false values
+    for (let y = 0; y < this.props.nrows; y++) {
+      let row = [];
+      for (let x = 0; x < this.props.ncols; x++) {
+        row.push(Math.random() < this.props.chanceLightStartsOn);
+      }
+      board.push(row);
+    }
     return board
   }
 
@@ -66,14 +79,27 @@ class Board extends Component {
     // win when every cell is turned off
     // TODO: determine is the game has been won
 
-    this.setState({board, hasWon});
+    // this.setState({board, hasWon});
   }
 
 
   /** Render game board or winning message. */
 
   render() {
+    let tblBoard = [];
+    for (let y = 0; y < this.props.nrows; y++) {
+      let row = [];
+      for (let x = 0; x < this.props.ncols; x++) {
+        let coord = `${y}-${x}`;
+        row.push(<Cell key={coord} isLit={this.state.board[y][x]} />);
+      }
+    tblBoard.push(<tr>{row}</tr>);
+    }
 
+    return (
+      <table className="Board">
+        <tbody>{tblBoard}</tbody>
+      </table>
     // if the game is won, just show a winning msg & render nothing else
 
     // TODO
@@ -81,6 +107,7 @@ class Board extends Component {
     // make table board
 
     // TODO
+    );
   }
 }
 
