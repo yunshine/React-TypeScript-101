cfeede4868b0b16f1e6568310376cf0614d3989c
Yun Chung <yun.chung@mac.com>
2020-07-21 12:11:51 +0900

    Added logic to display the answer and remove the buttons when the game ends

--- hangman/hangman-version-2/src/Hangman.js
+++ hangman/hangman-version-2/src/Hangman.js
@@ -58,13 +58,16 @@ class Hangman extends Component {
 
   /** render: render game */
   render() {
+    let gameOver = this.state.nWrong >= this.props.maxWrong;
     return (
       <div className='Hangman'>
         <h1>Hangman</h1>
         <img src={this.props.images[this.state.nWrong]} />
     <p>Number of Wrong Guess: {this.state.nWrong}</p>
-        <p className='Hangman-word'>{this.guessedWord()}</p>
-        <p className='Hangman-btns'>{this.generateButtons()}</p>
+        <p className='Hangman-word'>{!gameOver ? this.guessedWord() : this.state.answer}</p>
+        <p className='Hangman-btns'>
+          {gameOver ? `You lose. The correct answer was "${this.state.answer}".` : this.generateButtons()}
+          </p>
       </div>
     );
   }
