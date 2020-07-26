fefb23c5b680270e041790382893ae20835ced6d
Yun Chung <yun.chung@mac.com>
2020-07-26 18:10:55 +0900

    Fixed a typo that was preventing a css class from being dynamic

--- Yahtzee App/src/Dice.js
+++ Yahtzee App/src/Dice.js
@@ -10,7 +10,7 @@ class Dice extends Component {
           val={d}
           locked={this.props.locked[idx]}
           idx={idx}
-          disabled={this.props.disbled}
+          disabled={this.props.disabled}
           key={idx} />
       )}
     </div>
