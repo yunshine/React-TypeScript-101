baa71644e2643e2229619ad19afded209bb41a81
Yun Chung <yun.chung@mac.com>
2020-07-26 12:30:13 +0900

    Created a starter template for new React apps

---
+++ My React Template/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+// import -something-, { -something- } from './-something-';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Testing App Component...</h1>
+      {/* <-something- /> */}
+    </div>
+  );
+}
+
+export default App;
