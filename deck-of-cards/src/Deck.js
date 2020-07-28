ba7c01bbf9f019a25be37d228c797b50e1911b3f
Yun Chung <yun.chung@mac.com>
2020-07-28 11:36:38 +0900

    Created files for the Deck and Card components

---
+++ deck-of-cards/src/Deck.js
@@ -0,0 +1,38 @@
+import React, { Component } from 'react'; // imrc is the shortcut...
+// import -something-, { -something- } from './-something-';
+import './Deck.css'; // make a CSS file for this component...
+
+class Deck extends Component {
+  // static defaultProps = {
+  //   key: value,
+  // }
+  // constructor(props) {
+  //   super(props);
+    // this.state = { key: value };
+    // this.handleClick = this.handleClick.bind(this);
+  // }
+
+  // Deck() {
+  //   this.setState({ key: value });
+  // }
+
+  // handleClick() {
+  //   this.newFunction();
+  //   this.setState(oldState => {
+  //     return { score: oldState + 3 };
+  //   })
+  // }
+  // => This is the way and the syntax to update an existing state, not:   this.setState({ score: this.state.score + 3 });
+
+  render() {
+      return (
+      <div className="Deck">
+        {/* <h1>{this.state.Deck}</h1>
+        <h1>{this.props.Deck}</h1> */}
+        <h1>This is the Deck component...</h1>
+      </div>
+    );
+  }
+}
+
+export default Deck; /// connect this component to App.js...
