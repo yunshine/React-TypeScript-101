7ce8d559d853656973f05909f2856e3d75e709a4
Yun Chung <yun.chung@mac.com>
2020-07-29 10:04:15 +0900

    App CSS

--- dad-jokes/src/JokeList.js
+++ dad-jokes/src/JokeList.js
@@ -43,7 +43,10 @@ class JokeList extends Component {
 
     return (
       <div className="JokeList">
-        <h1>This is the JokeList component...</h1>
+        <div className="JokeList-sidebar">
+          <h1 className="JokeList-title"><span className="">Dad</span>Jokes</h1>
+          <img src='https://assets.dryicons.com/uploads/icon/svg/8927/0eb14c71-38f2-433a-bfc8-23d9c99b3647.svg' />
+        </div>
         <div className="JokeList-jokes">
           {this.state.jokes.map(j => (
           <div>{j}</div>))}
