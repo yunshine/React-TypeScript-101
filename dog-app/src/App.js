f77ceefd7a090a53c2453011151e7bd081ae832b
Yun Chung <yun.chung@mac.com>
2020-08-02 17:44:28 +0900

    Applied my React template to app

--- dog-app/src/App.js
+++ dog-app/src/App.js
@@ -1,24 +1,22 @@
 import React from 'react';
-import logo from './logo.svg';
+import { Route, Switch, NavLink } from 'react-router-dom';
+// import -something-, { -something- } from './-something-';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Testing App Component...</h1>
+      {/* <-something- /> */}
+
+      {/* <NavLink exact activeClassName="active-link" to="/-something-">-some-text-here-</NavLink> */}
+
+      {/* <Switch> */}
+        {/* use path="/" to assign a default page... */}
+        {/* <Route exact path="/-something-" component={-something-} /> */}
+        {/* <Route exact path="/-something-" component={() => <-something- name='Muffins"> } /> */}
+        {/* <Route exact path="/-something-" render={() => <-something- name='Biscuit"> } /> */}
+      {/* </Switch> */}
     </div>
   );
 }
