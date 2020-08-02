0a1cd9ad1dfe663ef2dd718be772457c1653a220
Yun Chung <yun.chung@mac.com>
2020-08-02 17:40:14 +0900

    Installed React Router to app

--- vending-machine-app/src/VendingMachine.js
+++ vending-machine-app/src/VendingMachine.js
@@ -3,32 +3,7 @@ import Message from './Message';
 import './VendingMachine.css'; // make a CSS file for this component...
 import { NavLink } from 'react-router-dom';
 
-
-// import { v4 as uuidv4 } from 'uuid'; // for creating unique IDs with uuidv4();
-// npm install axios (for API requests) in terminal???
-
 class VendingMachine extends Component {
-  // static defaultProps = {
-  //   key: value,
-  // };
-  
-  // constructor(props) {
-  //   super(props);
-    // this.state = { key: value };
-    // this.handleClick = this.handleClick.bind(this);
-  // }
-
-  // VendingMachine() {
-  //   this.setState({ key: value });
-  // }
-
-  // handleClick() {
-  //   this.newFunction();
-  //   this.setState(oldState => {
-  //     return { score: oldState + 3 };
-  //   })
-  // }
-  // => This is the way and the syntax to update an existing state, not:   this.setState({ score: this.state.score + 3 });
 
   render() {
     return (
