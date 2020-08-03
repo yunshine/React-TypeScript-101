b97d60d47f8a9c1653d214b50fb54db0674e96f6
Yun Chung <yun.chung@mac.com>
2020-08-03 14:23:46 +0900

    Refactored bootstrap container classes

--- dog-app/src/App.js
+++ dog-app/src/App.js
@@ -48,7 +48,9 @@ class App extends Component {
     return (
       <div className="App">
         <NavBar dogs={this.props.dogs} />
-        <Routes dogs={this.props.dogs} />
+        <div className="container">
+          <Routes dogs={this.props.dogs} />
+        </div>
       </div>
     );
   }
--- dog-app/src/DogList.js
+++ dog-app/src/DogList.js
@@ -32,16 +32,13 @@ class DogList extends Component {
   render() {
     return (
       <div className="DogList">
-        <h1 className="display-1 text-center">This is the DogList component...</h1>
-        <div className="container">
-          <div className="row">
-          {this.props.dogs.map(d => (
-            <div className="Dog col-md-6 col-lg-4 text-center" key={d.name}>
-              <img src={d.src} alt={d.name} />
-              <h3>{d.name}</h3>
-            </div>
-          ))}
+        <div className="row mt-3">
+        {this.props.dogs.map(d => (
+          <div className="Dog col-md-6 col-lg-4 text-center" key={d.name}>
+            <img src={d.src} alt={d.name} />
+            <h3>{d.name}</h3>
           </div>
+        ))}
         </div>
       </div>
     );
