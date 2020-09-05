8b78a559393ce3cdeabc829153f91df224a0f528
Yun Chung <yun.chung@mac.com>
2020-09-05 10:43:17 +0900

    Cache Busting and Plugins

--- webpack-demo-app/webpack.config.js
+++ webpack-demo-app/webpack.config.js
@@ -1,16 +1,27 @@
 const path = require("path");
+var HtmlWebpackPlugin = require("html-webpack-plugin");
+
 module.exports = {
   mode: "development",
   entry: "./src/index.js",
   output: {
-    filename: "main.js",
+    filename: "main.[contentHash].js",
     path: path.resolve(__dirname, "dist")
   },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: "./src/template.html"
+    })
+  ],
   module: {
     rules: [
       {
-        test: /\.css$/,
-        use: ["style-loader", "css-loader"]
+        test: /\.scss$/,
+        use: [
+          "style-loader", //3. Inject styles into DOM
+          "css-loader", //2. Turns css into commonjs
+          "sass-loader" //1. Turns sass into css
+        ]
       }
     ]
   }
