c50b708ab4acd276c512e42afbb2c539877bbbb6
Yun Chung <yun.chung@mac.com>
2020-09-26 17:39:40 +0900

    Able to display all tasks

--- to-do-basic/script.js
+++ to-do-basic/script.js
@@ -1,1 +1,16 @@
-// alert('connected!');
+let listItems = [
+  { task: 'Finish App', completed: false },
+  { task: 'Study Japanese', completed: false },
+  { task: 'Wash Dishes', completed: false },
+];
+
+const list = document.querySelector('ul');
+
+list.style.color = 'red';
+// list.insertAdjacentHTML('beforeend', '<li>France (2 wins)</li>');
+
+listItems.forEach((item) => {
+  console.log(item);
+  // list.innerHTML = `<li>${item.task}</li>`;
+  list.insertAdjacentHTML('beforeend', `<li>${item.task}</li>`);
+});
