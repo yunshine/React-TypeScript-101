d7f665b90c44bb2f9ec68fa4a607a52832287944
Yun Chung <yun.chung@mac.com>
2020-09-28 19:33:46 +0900

    Used jQuery to change checkbox icon when clicked

--- to-do-basic/script.js
+++ to-do-basic/script.js
@@ -4,5 +4,7 @@ function markDone(e) {}
 $('.far').on('click', function (e) {
   console.log($(this).parent());
   $(this).parent().toggleClass('done');
+  $(this).attr('class', 'far fa-check-square');
+  // $(this).remove();
   e.stopPropagation;
 });
