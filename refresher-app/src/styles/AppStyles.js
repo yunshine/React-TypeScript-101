1e6e4260fc2b043e93755a14e190605c7cd04a42
Yun Chung <yun.chung@mac.com>
2020-12-29 16:16:16 +0900

    Added purple box shadow to Refresher App app component

--- refresher-app/src/styles/AppStyles.js
+++ refresher-app/src/styles/AppStyles.js
@@ -1,13 +1,11 @@
 // import sizes from './MediaSizes';
 
-import { grey } from "@material-ui/core/colors";
-
 export default {
     App: {
         margin: 'auto',
         width: '480px',
         textAlign: 'center',
-        boxShadow: '0px 0px 740px -40px grey',
+        boxShadow: '0px 0px 740px -40px purple',
         '& h1': {
             color: 'red',
             fontSize: '40px',
