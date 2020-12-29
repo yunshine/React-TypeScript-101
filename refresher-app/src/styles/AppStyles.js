daaccb13bfaa88b7c5b6e66fc45f60a255e1796a
Yun Chung <yun.chung@mac.com>
2020-12-29 16:13:12 +0900

    Added box shadow to Refresher App app component

--- refresher-app/src/styles/AppStyles.js
+++ refresher-app/src/styles/AppStyles.js
@@ -1,9 +1,13 @@
 // import sizes from './MediaSizes';
 
+import { grey } from "@material-ui/core/colors";
+
 export default {
     App: {
         margin: 'auto',
+        width: '480px',
         textAlign: 'center',
+        boxShadow: '0px 0px 740px -40px grey',
         '& h1': {
             color: 'red',
             fontSize: '40px',
