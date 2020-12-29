71533d30b4b43d6db5fb7a4e69b3acdf035195f4
Yun Chung <yun.chung@mac.com>
2020-12-29 14:47:07 +0900

    Added Material-UI classes to Refresher App Machine component

--- refresher-app/src/Machine.js
+++ refresher-app/src/Machine.js
@@ -1,17 +1,19 @@
 import React, { Component } from 'react';
+import { withStyles } from '@material-ui/styles';
+import styles from './styles/MachineStyles';
 
 class Machine extends Component {
     render() {
-        const { s1, s2, s3 } = this.props;
+        const { s1, s2, s3, classes } = this.props;
         let isWinner = (s1 === s2) && (s2 === s3);
         return (
-            <div>
-                <h2>{s1}  |  {s2}  |  {s3}</h2>
-                <h2>{isWinner ? "Winner!" : "Loser 😭"}</h2>
+            <div className={classes.Machine}>
+                <h2 className={classes.MachineSH2}>{s1}  |  {s2}  |  {s3}</h2>
+                <h2 className={classes.MachineResultText}>{isWinner ? "Winner!" : "Loser 😭"}</h2>
                 <br></br>
             </div>
         );
     }
 }
 
-export default Machine;
+export default withStyles(styles)(Machine);
