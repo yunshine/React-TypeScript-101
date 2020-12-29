e9062ea37fd79a86814706d45bdf3d1e0574836a
Yun Chung <yun.chung@mac.com>
2020-12-29 15:17:08 +0900

    Added Mateial-UI class to Refresher App component

--- refresher-app/src/App.js
+++ refresher-app/src/App.js
@@ -1,13 +1,13 @@
 import React, { Component } from 'react';
 // import { Route, Switch, NavLink } from 'react-router-dom';
-// import -something-, { -something- } from './-something-';
+import { withStyles } from '@material-ui/styles';
+import styles from './styles/AppStyles';
 import Machine from './Machine';
-import './App.css';
 
 class App extends Component {
     render() {
         return (
-            <div className="App">
+            <div className={this.props.classes.App}>
                 <h1>Play the Slots!</h1>
                 <Machine s1="😆" s2="😷" s3="❤️" />
                 <Machine s1="👍🏻" s2="👍🏻" s3="👍🏻" />
@@ -17,4 +17,4 @@ class App extends Component {
     }
 }
 
-export default App;
+export default withStyles(styles)(App);
