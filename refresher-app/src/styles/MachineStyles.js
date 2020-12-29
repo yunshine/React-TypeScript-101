0c1019b43e5b8ad09beaa255c7dfb29c2682b1fc
Yun Chung <yun.chung@mac.com>
2020-12-29 15:08:38 +0900

    Added Media Queries to Refresher App

---
+++ refresher-app/src/styles/MachineStyles.js
@@ -0,0 +1,38 @@
+// import sizes from './MediaSizes';
+
+export default {
+    // About: {
+    //     margin: 'auto',
+    //     marginTop: '0px',
+    //     width: '56vw',
+    //     // border: '1px solid blue',
+    //     // textAlign: 'center',
+    //     [sizes.down('md')]: {
+    //         display: 'block',
+    //         width: '85vw',
+    //         margin: 'auto',
+    //         padding: '0px 16px',
+    //     },
+    //     [sizes.down('sm')]: {
+    //         display: 'block',
+    //         width: '88vw',
+    //         margin: 'auto',
+    //     },
+    // },
+    // AboutTitle: {
+    //     '& h1': {
+    //         textAlign: 'center',
+    //         color: 'rgba(20, 15, 73, 0.32)',
+    //         fontSize: '32px',
+    //         marginBottom: '0px',
+    //         lineHeight: '85%',
+    //         fontWeight: '350',
+    //     },
+    //     '& h2': {
+    //         textAlign: 'center',
+    //         color: 'rgba(20, 15, 73, 1)',
+    //         fontSize: '56px',
+    //         fontWeight: '550',
+    //     },
+    // },
+};
