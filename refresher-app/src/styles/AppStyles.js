28026c972054dc15c88be12f26527b7fe8195779
Yun Chung <yun.chung@mac.com>
2020-12-29 15:55:14 +0900

    Styled Refresher App App component

--- refresher-app/src/styles/AppStyles.js
+++ refresher-app/src/styles/AppStyles.js
@@ -1,38 +1,13 @@
 // import sizes from './MediaSizes';
 
 export default {
-    // About: {
-    //     margin: 'auto',
-    //     marginTop: '0px',
-    //     width: '56vw',
-    //     // border: '1px solid blue',
-    //     // textAlign: 'center',
-    //     [sizes.down('md')]: {
-    //         display: 'block',
-    //         width: '85vw',
-    //         margin: 'auto',
-    //         padding: '0px 16px',
-    //     },
-    //     [sizes.down('sm')]: {
-    //         display: 'block',
-    //         width: '88vw',
-    //         margin: 'auto',
-    //     },
-    // },
-    // AboutTitle: {
-    //     '& h1': {
-    //         textAlign: 'center',
-    //         color: 'rgba(20, 15, 73, 0.32)',
-    //         fontSize: '32px',
-    //         marginBottom: '0px',
-    //         lineHeight: '85%',
-    //         fontWeight: '350',
-    //     },
-    //     '& h2': {
-    //         textAlign: 'center',
-    //         color: 'rgba(20, 15, 73, 1)',
-    //         fontSize: '56px',
-    //         fontWeight: '550',
-    //     },
-    // },
+    App: {
+        margin: 'auto',
+        textAlign: 'center',
+        '& h1': {
+            color: 'red',
+            fontSize: '40px',
+            fontWeight: '800',
+        }
+    },
 };
