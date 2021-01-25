4a08e816ec26e6967dd7bd8ee8517cd5692d972f
Yun Chung <yun.chung@mac.com>
2021-01-25 12:34:00 +0900

    Lines of code on TypeScript classes

--- TypeScript-Apps/4. TypeScript Classes/classes.js
+++ TypeScript-Apps/4. TypeScript Classes/classes.js
@@ -1,12 +1,22 @@
 var Cafe = /** @class */ (function () {
-    // 'Coffee' is now a default value for 'specialty'...
-    function Cafe() {
+    // When a new Cafe instance is created, a name and area must be passed as arguments AND they must be strings...
+    function Cafe(name, area) {
+        // 'Unknown' is now a default value for 'name'...
+        this.name = "Unknown";
         // 'Tokyo' is now a default value for 'area'...
         this.area = "Tokyo";
-        this.specialty = "Coffee";
+        this.name = name;
+        // Another way to set 'Tokyo' as a default value for 'area'...
+        this.area = area = "Tokyo";
+        // this.specialty = specialty;
     }
+    // static properties are only accessible on the class model (ie. Cafe)...
+    Cafe.castle = "Winterfell";
     return Cafe;
 }());
-var cafeKitsune = new Cafe();
-cafeKitsune.area = "Aoyama";
+// let cafeKitsune = new Cafe();
+var cafeKitsune = new Cafe("Cafe Kitsune", "Aoyama");
+// Error because a string was not passed as a specialty...
+cafeKitsune.specialty = 12;
 console.log('cafeKitsune: ', cafeKitsune);
+console.log('Cafe castle: ', Cafe.castle);
--- TypeScript-Apps/4. TypeScript Classes/classes.ts
+++ TypeScript-Apps/4. TypeScript Classes/classes.ts
@@ -1,17 +1,28 @@
 class Cafe {
-    name: string;
+    // 'Unknown' is now a default value for 'name'...
+    name: string = "Unknown";
     // 'Tokyo' is now a default value for 'area'...
     area: string = "Tokyo";
+    // 'specialty' must be a string...
     specialty: string;
+
+    // static properties are only accessible on the class model (ie. Cafe)...
+    static castle: string = "Winterfell";
     
-    // 'Coffee' is now a default value for 'specialty'...
-    constructor() {
-        this.specialty = "Coffee";
+    // When a new Cafe instance is created, a name and area must be passed as arguments AND they must be strings...
+    constructor(name, area) {
+        this.name = name;
+        // Another way to set 'Tokyo' as a default value for 'area'...
+        this.area = area = "Tokyo";
+        // this.specialty = specialty;
     }
 }
 
-let cafeKitsune = new Cafe();
+// let cafeKitsune = new Cafe();
+let cafeKitsune = new Cafe("Cafe Kitsune", "Aoyama");
 
-cafeKitsune.area = "Aoyama";
+// Error because a string was not passed as a specialty...
+cafeKitsune.specialty = 12;
 
-console.log('cafeKitsune: ', cafeKitsune)
+console.log('cafeKitsune: ', cafeKitsune);
+console.log('Cafe castle: ', Cafe.castle);
