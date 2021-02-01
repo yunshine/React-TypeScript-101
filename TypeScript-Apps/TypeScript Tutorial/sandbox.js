db95bc6a3497381101df20e650a4a0fd7a52c47a
Yun Chung <yun.chung@mac.com>
2021-02-01 16:42:07 +0900

    TypeScript types lesson

--- TypeScript-Apps/TypeScript Tutorial/sandbox.js
+++ TypeScript-Apps/TypeScript Tutorial/sandbox.js
@@ -1,7 +1,27 @@
 // typing tsc sandbox.ts -w will keep "watching" this file and compile to sandbox.js whenever a change has been detected...
-var character = 'Luigi';
+var character = 'mario';
+var age = 30;
+var isBlackBelt = false;
+// character = 20;
+character = 'luigi';
+// age = 'yoshi';
+age = 40;
+// isBlackBelt = 'yes';
+isBlackBelt = true;
+var circumference = function (diameter) {
+    return diameter * Math.PI;
+};
+// console.log(circumference('hello'));
+console.log(circumference(7.5));
+/*
+Notes From Lesson 2--------------------------------------------------------
+const character = 'Luigi';
+
 console.log("Character Name: ", character);
-var inputs = document.querySelectorAll('input');
-inputs.forEach(function (input) {
+
+const inputs = document.querySelectorAll('input');
+
+inputs.forEach(input => {
     console.log("All Inputs: ", input);
 });
+*/ 
--- TypeScript-Apps/TypeScript Tutorial/sandbox.ts
+++ TypeScript-Apps/TypeScript Tutorial/sandbox.ts
@@ -1,5 +1,27 @@
 // typing tsc sandbox.ts -w will keep "watching" this file and compile to sandbox.js whenever a change has been detected...
 
+let character = 'mario';
+let age = 30;
+let isBlackBelt = false;
+
+// character = 20;
+character = 'luigi';
+
+// age = 'yoshi';
+age = 40;
+
+// isBlackBelt = 'yes';
+isBlackBelt = true;
+
+const circumference = (diameter: number) => {
+    return diameter * Math.PI;
+};
+
+// console.log(circumference('hello'));
+console.log(circumference(7.5));
+
+/*
+Notes From Lesson 2--------------------------------------------------------
 const character = 'Luigi';
 
 console.log("Character Name: ", character);
@@ -9,3 +31,4 @@ const inputs = document.querySelectorAll('input');
 inputs.forEach(input => {
     console.log("All Inputs: ", input);
 });
+*/
