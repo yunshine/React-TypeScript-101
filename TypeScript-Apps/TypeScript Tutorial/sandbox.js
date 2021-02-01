bd8b4530667faf156bda69f9b80377de43031f55
Yun Chung <yun.chung@mac.com>
2021-02-01 17:11:12 +0900

    TypeScript arrays and objects

--- TypeScript-Apps/TypeScript Tutorial/sandbox.js
+++ TypeScript-Apps/TypeScript Tutorial/sandbox.js
@@ -1,18 +1,59 @@
 // typing tsc sandbox.ts -w will keep "watching" this file and compile to sandbox.js whenever a change has been detected...
-var character = 'mario';
-var age = 30;
-var isBlackBelt = false;
+// TypeScipt Arrays
+// if we declare/initialize an array with only one type, it cannot have other types in it...
+var names = ['luigi', 'mario', 'yoshi'];
+// names.push(3);
+// names[1] = 3;
+names.push('toad');
+var numbers = [10, 20, 12, 15];
+// numbers.push('shaun');
+// numbers[0] = 'shaun';
+numbers.push(25);
+// this array can have strings and numbers but not booleans  in in because it was declared/initialized with strings and numbers  in it but not booleans...
+var mixed = ['ken', 4, 'chun-li', 8, 9];
+// mixed.push(true);
+mixed.push('ryu');
+mixed.push(10);
+mixed[0] = 3;
+// TypeScript Objects
+// in this TypeScript example, 'ninja' must always be an object, 'name' and 'belt' must always be strings, and 'age' must always be a number...
+var ninja = {
+    name: 'mario',
+    belt: 'black',
+    age: 30
+};
+// ninja.age = '30';
+ninja.age = 40;
+ninja.name = 'ryu';
+// in TypeScript, we can't add a new property to the ninja object like 'skills'...
+// ninja.skills = ['fighting', 'sneaking']
+ninja = {
+    name: 'yoshi',
+    belt: 'orange',
+    age: 40
+};
+/*
+Notes From Lesson --------------------------------------------------------
+let character = 'mario';
+let age = 30;
+let isBlackBelt = false;
+
 // character = 20;
 character = 'luigi';
+
 // age = 'yoshi';
 age = 40;
+
 // isBlackBelt = 'yes';
 isBlackBelt = true;
-var circumference = function (diameter) {
+
+const circumference = (diameter: number) => {
     return diameter * Math.PI;
 };
+
 // console.log(circumference('hello'));
 console.log(circumference(7.5));
+*/
 /*
 Notes From Lesson 2--------------------------------------------------------
 const character = 'Luigi';
@@ -24,4 +65,4 @@ const inputs = document.querySelectorAll('input');
 inputs.forEach(input => {
     console.log("All Inputs: ", input);
 });
-*/ 
+*/
--- TypeScript-Apps/TypeScript Tutorial/sandbox.ts
+++ TypeScript-Apps/TypeScript Tutorial/sandbox.ts
@@ -1,5 +1,51 @@
 // typing tsc sandbox.ts -w will keep "watching" this file and compile to sandbox.js whenever a change has been detected...
 
+// TypeScipt Arrays
+// if we declare/initialize an array with only one type, it cannot have other types in it...
+const names = ['luigi', 'mario', 'yoshi'];
+
+// names.push(3);
+// names[1] = 3;
+names.push('toad');
+
+const numbers = [10, 20, 12, 15];
+
+// numbers.push('shaun');
+// numbers[0] = 'shaun';
+numbers.push(25);
+
+// this array can have strings and numbers but not booleans  in in because it was declared/initialized with strings and numbers  in it but not booleans...
+const mixed = ['ken', 4, 'chun-li', 8, 9];
+
+// mixed.push(true);
+mixed.push('ryu');
+mixed.push(10);
+mixed[0] = 3;
+
+// TypeScript Objects
+// in this TypeScript example, 'ninja' must always be an object, 'name' and 'belt' must always be strings, and 'age' must always be a number...
+let ninja = {
+    name: 'mario',
+    belt: 'black',
+    age: 30
+};
+
+// ninja.age = '30';
+ninja.age = 40;
+ninja.name = 'ryu';
+// in TypeScript, we can't add a new property to the ninja object like 'skills'...
+// ninja.skills = ['fighting', 'sneaking']
+
+ninja = {
+    name: 'yoshi',
+    belt: 'orange',
+    age: 40,
+    // skills: ['running'],
+};
+
+
+/*
+Notes From Lesson --------------------------------------------------------
 let character = 'mario';
 let age = 30;
 let isBlackBelt = false;
@@ -19,6 +65,8 @@ const circumference = (diameter: number) => {
 
 // console.log(circumference('hello'));
 console.log(circumference(7.5));
+*/
+
 
 /*
 Notes From Lesson 2--------------------------------------------------------
