961cd42a22015e070a61b4a69ddf921d3b4da571
Yun Chung <yun.chung@mac.com>
2021-02-04 17:09:32 +0900

    TypeScript classes

--- TypeScript-Apps/TypeScript Tutorial/public/app.js
+++ TypeScript-Apps/TypeScript Tutorial/public/app.js
@@ -1,12 +1,35 @@
 "use strict";
+// Lesson 12: TypeScript Classes------------------------------------------
+var Invoice = /** @class */ (function () {
+    function Invoice(client, details, amount) {
+        this.client = client;
+        this.details = details;
+        this.amount = amount;
+    }
+    Invoice.prototype.format = function () {
+        return this.client + " owes $" + this.amount + " for " + this.details + ".";
+    };
+    return Invoice;
+}());
+var invoiceTestOne = new Invoice("Yun", "sneakers", 200);
+var invoiceTestTwo = new Invoice("Eunjoo", "snacks", 15);
+console.log(invoiceTestOne.format());
+console.log(invoiceTestTwo.format());
+// below, only objects using the Invoice class can be added to the invoices array...
+var invoices = [];
+// invoices.push("hello");
+invoices.push(invoiceTestOne);
+invoices.push(invoiceTestTwo);
+console.log("Invoice objects only: ", invoices);
+// Lesson 11: TypeScript TypeCasting------------------------------------------
 var anchorTag = document.querySelector('a');
-// the console.log works, but there is a TS warning that the href could possibly be null. So, you can add a ! at the end of line 1 to tell TS that I KNOW that this querySelector will return a value...
+// the console.log below will work, but there is a TS warning that the href could possibly be null. So, you can add a ! at the end of line 1 to tell TS that I KNOW that this querySelector will return a value...
 console.log("Testing TypeScript: ", anchorTag.href);
-// Lesson 11: TypeScript TypeCasting-----------------------------------
 // const form = document.querySelector('form')!;
-// now that I've told TS taht this is an HTMLFormElement, I have all the warnings and autocompletions associated with HTMLFormElements...
 var form = document.querySelector('.new-item-form');
+// now that I've told TS that this is an HTMLFormElement, I have all the warnings and autocompletions associated with HTMLFormElements...
 // console.log(form.children);
+// App Inputs
 var typeField = document.querySelector('#type');
 var toFromField = document.querySelector('#tofrom');
 var detailsField = document.querySelector('#details');
--- TypeScript-Apps/TypeScript Tutorial/src/app.ts
+++ TypeScript-Apps/TypeScript Tutorial/src/app.ts
@@ -1,14 +1,45 @@
+// Lesson 12: TypeScript Classes------------------------------------------
+class Invoice {
+    client: string;
+    details: string;
+    amount: number;
+
+    constructor(client: string, details: string, amount: number) {
+        this.client = client;
+        this.details = details;
+        this.amount = amount;
+    }
+    format() {
+        return `${this.client} owes $${this.amount} for ${this.details}.`;
+    }
+}
+
+const invoiceTestOne = new Invoice("Yun", "sneakers", 200);
+const invoiceTestTwo = new Invoice("Eunjoo", "snacks", 15);
+console.log(invoiceTestOne.format());
+console.log(invoiceTestTwo.format());
+
+// below, only objects using the Invoice class can be added to the invoices array...
+let invoices: Invoice[] = [];
+// invoices.push("hello");
+invoices.push(invoiceTestOne);
+invoices.push(invoiceTestTwo);
+console.log("Invoice objects only: ", invoices);
+
+
+
+// Lesson 11: TypeScript TypeCasting------------------------------------------
 const anchorTag = document.querySelector('a')!;
 
-// the console.log works, but there is a TS warning that the href could possibly be null. So, you can add a ! at the end of line 1 to tell TS that I KNOW that this querySelector will return a value...
+// the console.log below will work, but there is a TS warning that the href could possibly be null. So, you can add a ! at the end of line 1 to tell TS that I KNOW that this querySelector will return a value...
 console.log("Testing TypeScript: ", anchorTag.href);
 
-// Lesson 11: TypeScript TypeCasting-----------------------------------
 // const form = document.querySelector('form')!;
-// now that I've told TS taht this is an HTMLFormElement, I have all the warnings and autocompletions associated with HTMLFormElements...
 const form = document.querySelector('.new-item-form') as HTMLFormElement;
+// now that I've told TS that this is an HTMLFormElement, I have all the warnings and autocompletions associated with HTMLFormElements...
 // console.log(form.children);
 
+// App Inputs
 const typeField = document.querySelector('#type') as HTMLSelectElement;
 const toFromField = document.querySelector('#tofrom') as HTMLInputElement;
 const detailsField = document.querySelector('#details') as HTMLInputElement;
