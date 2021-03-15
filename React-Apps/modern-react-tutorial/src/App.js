e073be16d78aa71d2f5aa34d34accb219e8c6180
Yun Chung <yun.chung@mac.com>
2021-03-15 13:18:16 +0900

    Added an example of dynamic values in React templates

--- React-Apps/modern-react-tutorial/src/App.js
+++ React-Apps/modern-react-tutorial/src/App.js
@@ -1,10 +1,18 @@
 import './App.css';
 
 function App() {
+    const title = 'This is the title in the App component...';
+    let today = new Date();
+    const dd = String(today.getDate()).padStart(2, '0');
+    const mm = String(today.getMonth() + 1).padStart(2, '0'); //January is 0!
+    const yyyy = today.getFullYear();
+    today = mm + '/' + dd + '/' + yyyy;
+
     return (
         <div className="App">
             <div className="content">
-                <h1>This is the App component...</h1>
+                <h1>{title}</h1>
+                <h2>Today's date: {today}</h2>
             </div>
         </div>
     );
