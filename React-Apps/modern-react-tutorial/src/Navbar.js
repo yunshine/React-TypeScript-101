eadecf136f25be04d18fd950fecad263a291df36
Yun Chung <yun.chung@mac.com>
2021-03-15 18:08:12 +0900

    Commented out example of inline styling in React

--- React-Apps/modern-react-tutorial/src/Navbar.js
+++ React-Apps/modern-react-tutorial/src/Navbar.js
@@ -5,11 +5,13 @@ function Navbar() {
             <h1>Yun's React Blog</h1>
             <div className="links">
                 <a href="/">Home</a>
-                <a href="/create" style={{
-                    color: "white",
-                    backgroundColor: "#f1356d",
-                    borderRadius: "8px"
-                }}>New Post</a>
+                <a href="/create"
+                // style={{
+                //     color: "white",
+                //     backgroundColor: "#f1356d",
+                //     borderRadius: "8px"
+                // }}
+                >New Post</a>
             </div>
         </nav>
     );
