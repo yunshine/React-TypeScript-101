422c7fef2a986087a99d3bb557b4ef4108e8cc55
Yun Chung <yun.chung@mac.com>
2021-03-18 15:11:04 +0900

    Added notes and an example of passing a function down as a prop

--- React-Apps/modern-react-tutorial/src/BlogList.js
+++ React-Apps/modern-react-tutorial/src/BlogList.js
@@ -8,6 +8,10 @@ const BlogList = (props) => {
                 <div className="blog-preview" key={blog.id}>
                     <h2>{blog.title}</h2>
                     <p>Written By: {blog.author}</p>
+                    {/* how to delete a blog... */}
+                    {/* in the onClick, I'm using an anonymous function so that we can pass an argument... */}
+                    {/* the handleDelete function should be in the home component and passed down as a prop because that's where the data is... */}
+                    <button onClick={() => props.handleDelete(blog.id)}>Delete Blog</button>
                 </div>
             ))}
         </div>
--- React-Apps/modern-react-tutorial/src/Home.js
+++ React-Apps/modern-react-tutorial/src/Home.js
@@ -8,10 +8,15 @@ const Home = () => {
         { title: "Date Night", body: "lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum...", author: "Eunjoo", id: 3 }
     ]);
 
+    function handleDelete(id) {
+        const newBlogs = blogs.filter(blog => id !== blog.id);
+        setBlogs(newBlogs);
+    }
+
     return (
         <div className="home">
-            <BlogList blogs={blogs} title="All Blogs" />
-            <BlogList blogs={blogs.filter(blog => blog.author === "Eunjoo")} title="Eunjoo's Blogs" />
+            <BlogList blogs={blogs} title="All Blogs" handleDelete={handleDelete} />
+            {/* <BlogList blogs={blogs.filter(blog => blog.author === "Eunjoo")} title="Eunjoo's Blogs" /> */}
         </div>
     );
 }
