79e5f8db2d646d96b62378a97a37addc1fc337ae
Yun Chung <yun.chung@mac.com>
2021-03-20 13:23:26 +0900

    Added notes and example of useEffect dependencies

--- React-Apps/modern-react-tutorial/src/Home.js
+++ React-Apps/modern-react-tutorial/src/Home.js
@@ -8,6 +8,8 @@ const Home = () => {
         { title: "Date Night", body: "lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum...", author: "Eunjoo", id: 3 }
     ]);
 
+    const [name, setName] = useState("Yun");
+
     function handleDelete(id) {
         const newBlogs = blogs.filter(blog => id !== blog.id);
         setBlogs(newBlogs);
@@ -15,14 +17,17 @@ const Home = () => {
 
     // the useEffect hook runs a function at every render of the component such as when it first loads and/or when the state changes...
     // to use useEffect, pass it a function. It will run this function at every render...
+    // the second argument of the useEffect hook, called a dependency array, allows you to choose which renders to run this particular useEffect hook...
     useEffect(() => {
         console.log("there was a render that occurred, and useEffect ran...");
-    });
+    }, [blogs]);
+    // dependency array options: [ ] an empty array like this will run the useEffect hook on only the initial render; [name] useEffect runs when the value for 'name' changes; [blogs] useEffect runs when the value for 'blogs' changes...
 
     return (
         <div className="home">
             <BlogList blogs={blogs} title="All Blogs" handleDelete={handleDelete} />
             {/* <BlogList blogs={blogs.filter(blog => blog.author === "Eunjoo")} title="Eunjoo's Blogs" /> */}
+            <button onClick={() => setName("Eunjoo")}>change name</button>
         </div>
     );
 }
