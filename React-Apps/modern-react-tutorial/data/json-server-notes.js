3af1f89ddffabf0a27164c50d47d85a618fd5b3e
Yun Chung <yun.chung@mac.com>
2021-03-21 14:11:03 +0900

    Added notes for using json-server

---
+++ React-Apps/modern-react-tutorial/data/json-server-notes.js
@@ -0,0 +1,8 @@
+// npx json-server --watch data/db.json --watch -port 8080
+
+/*
+/blogs           GET            fetch all blogs
+/blog/:id       GET             fetch a single blog
+/blogs          POST           add a new blog
+/blogs/:id     DELETE       delete a blog
+*/
