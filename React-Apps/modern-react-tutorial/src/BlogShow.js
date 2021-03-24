cf33cd00fa4fb7e98ff289af123f8b72a1591384
Yun Chung <yun.chung@mac.com>
2021-03-24 12:40:49 +0900

    Added useFetch hook, template structure, and styles to blog show component component

--- React-Apps/modern-react-tutorial/src/BlogShow.js
+++ React-Apps/modern-react-tutorial/src/BlogShow.js
@@ -1,11 +1,23 @@
 import { useParams } from "react-router";
+import useFetch from './useFetch';
 
 const BlogShow = () => {
     // the useParams hook - how to get the id from the url...
     const { id } = useParams();
+    const { data: blog, isPending, error } = useFetch('http://localhost:8080/blogs/' + id);
+
+
     return (
         <div className="BlogShow">
-            <h2>Blog Details Page... Blog ID: {id}</h2>
+            {isPending && <div>Loading...</div>}
+            {error && <div>{error}</div>}
+            {blog && (
+                <article>
+                    <h2>{blog.title}</h2>
+                    <p>Written By: {blog.author}</p>
+                    <h3>{blog.body}</h3>
+                </article>
+            )}
         </div>
     );
 }
