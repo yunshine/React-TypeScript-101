94199533d497a9e425000f60d3c18afb5baaa014
Yun Chung <yun.chung@mac.com>
2021-03-25 10:29:48 +0900

    Added form for creating a new blog post

--- React-Apps/modern-react-tutorial/src/Create.js
+++ React-Apps/modern-react-tutorial/src/Create.js
@@ -1,6 +1,27 @@
 const Create = () => {
     return (
-        <h1>hi</h1>
+        <div className="Create">
+            <h1>Create a New Blog Post</h1>
+            <form>
+                <label>Blog Title</label>
+                <input
+                    type="text"
+                    required
+                />
+
+                <label>Blog Body</label>
+                <textarea
+                    required
+                ></textarea>
+
+                <label>Blog Author: </label>
+                <select>
+                    <option value="Eunjoo">Eunjoo</option>
+                    <option value="Yun">Yun</option>
+                </select>
+                <button>Submit Blog</button>
+            </form>
+        </div>
     );
 }
 
