0c2edc38b9a74e8cf92067a08f141ae24ac7dfb1
Yun Chung <yun.chung@mac.com>
2021-03-29 17:11:01 +0900

    Added delete button and handleDelete function without logic

--- React-Apps/sneaker-collection/src/SneakerShow.js
+++ React-Apps/sneaker-collection/src/SneakerShow.js
@@ -6,6 +6,10 @@ const SneakerShow = () => {
 
     const { data: sneaker, isPending, error } = useFetch('http://localhost:8080/sneakers/' + id);
 
+    const handleDelete = () => {
+        console.log("delete id: ", id);
+    }
+
     return (
         <div className="SneakerShow">
             <p>this is the sneakershow component for sneaker id number {id}</p>
@@ -13,6 +17,7 @@ const SneakerShow = () => {
             {error && <h1>{error.message}</h1>}
             {sneaker && <h2>{sneaker.name}</h2>}
             {sneaker && <img src={sneaker.photo} className="SneakerPhoto" alt={`${sneaker.brand} ${sneaker.name}`} />}
+            <button onClick={handleDelete}>Delete This Sneaker</button>
         </div>
     );
 }
