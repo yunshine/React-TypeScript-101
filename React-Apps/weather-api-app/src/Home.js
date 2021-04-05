fffeff88779c15bdf1a444ca58d0fe5c106756d1
Yun Chung <yun.chung@mac.com>
2021-04-05 15:49:23 +0900

    Added form to home component

--- React-Apps/weather-api-app/src/Home.js
+++ React-Apps/weather-api-app/src/Home.js
@@ -1,7 +1,28 @@
+import { useState } from 'react';
+
 const Home = () => {
+    const [city, setCity] = useState('');
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        console.log(e);
+    }
+
     return (
         <div className="Home">
             this is the home component...
+            <form onSubmit={handleSubmit}>
+                <label>City</label>
+                {/* how to use forms in React... */}
+                <input
+                    type="text"
+                    required
+                    value={city}
+                    onChange={(e) => setCity(e.target.value)}
+                />
+                <h1>{city}</h1>
+                <button>submit city</button>
+            </form>
         </div>
     );
 }
