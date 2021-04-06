69327d0ad7840b7cfdb69cd15fef3a3f96a52cb2
Yun Chung <yun.chung@mac.com>
2021-04-06 12:43:28 +0900

    Able to access api using key in .env file

--- React-Apps/weather-api-app/src/Home.js
+++ React-Apps/weather-api-app/src/Home.js
@@ -1,58 +1,58 @@
 import { useState } from 'react';
 
+require('dotenv').config();
+
 const Home = () => {
     const [city, setCity] = useState('');
     const [data, setData] = useState(null);
     const [isPending, setIsPending] = useState(true);
     const [error, setError] = useState(null);
 
-
     const handleSubmit = (e) => {
         e.preventDefault();
 
-        // let url = ;
+        let url = `http://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${process.env.REACT_APP_API_KEY}`;
 
         console.log(url);
 
-        fetch(url)
-            .then(res => {
-                if (!res.ok) {
-                    console.log("fetching...");
+        setTimeout(() => {
+            fetch(url)
+                .then(res => {
+                    if (!res.ok) {
+                        console.log("fetching...");
 
-                    throw Error('There was an error, and data could not be fetched...');
-                }
-                return res.json();
-            })
-            .then(data => {
-                setData(data);
-                setIsPending(false);
-                setError(null);
-                console.log(data);
-            })
-            .catch(err => {
-                if (err.name === 'AbortError') {
-                    console.log("This fetch request has been aborted by abortController...");
-                } else {
+                        throw Error('There was an error, and data could not be fetched...');
+                    }
+                    return res.json();
+                })
+                .then(data => {
+                    setData(data);
                     setIsPending(false);
-                    setError(err.message);
-                    console.log("there was an error", err);
-                }
-            });
+                    setError(null);
+                    console.log(data);
+                })
+                .catch(err => {
+                    if (err.name === 'AbortError') {
+                        console.log("This fetch request has been aborted by abortController...");
+                    } else {
+                        setIsPending(false);
+                        setError(err.message);
+                        console.log("there was an error...", err);
+                    }
+                });
+        }, 850);
     }
 
     return (
         <div className="Home">
-            this is the home component...
             <form onSubmit={handleSubmit}>
                 <label>City</label>
-                {/* how to use forms in React... */}
                 <input
                     type="text"
                     required
                     value={city}
                     onChange={(e) => setCity(e.target.value)}
                 />
-                <h1>{city}</h1>
                 <button>submit city</button>
             </form>
         </div>
