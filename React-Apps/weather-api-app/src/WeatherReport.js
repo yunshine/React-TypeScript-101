41381706ea977152eafad2f7f6e3660387a924f0
Yun Chung <yun.chung@mac.com>
2021-04-07 11:34:09 +0900

    Able to display weather icon

--- React-Apps/weather-api-app/src/WeatherReport.js
+++ React-Apps/weather-api-app/src/WeatherReport.js
@@ -4,6 +4,7 @@ const WeatherReport = (props) => {
             <h2>this is the weather report:</h2>
             <h2>{props.weatherData.weather[0].description}</h2>
             <h2>{(props.weatherData.main.temp - 273.15).toFixed(1)} C</h2>
+            <img src="http://openweathermap.org/img/w/10d.png" alt="weather" />
         </div>
     );
 }
