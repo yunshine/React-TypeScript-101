dffb231d077898f37b624fbddfa470f9013a8e10
Yun Chung <yun.chung@mac.com>
2021-04-13 16:20:50 +0900

    Added passport-jwt and authorization middleware

--- React-Apps/mern-authentication/passport.js
+++ React-Apps/mern-authentication/passport.js
@@ -1,7 +1,34 @@
 const passport = require('passport');
 const LocalStrategy = require('passport-local').Strategy;
+const JwtStrategy = require('passport-jwt').Strategy;
 const User = require('./models/User');
 
+const cookieExtractor = req => {
+    let token = null;
+    if (req && req.cookies) {
+        token = req.cookies["access_token"];
+    }
+    return token;
+}
+
+// used for authorization when protecting a resource...
+passport.use(new JwtStrategy({
+    jwtFromRequest: cookieExtractor,
+    secretOrKey: "mernauth"
+}, (payload, done) => {
+    User.findById({ _id: payload.sub }, (err, user) => {
+        if (err) {
+            return done(err, false);
+        }
+        if (user) {
+            return done(null, user);
+        } else {
+            return done(null, false);
+        }
+    });
+}));
+
+//  used to authenticate using username and password when logging in...
 passport.use(new LocalStrategy((username, password, done) => {
     User.findOne({ username }, (err, user) => {
         // something went wrong with the database...
