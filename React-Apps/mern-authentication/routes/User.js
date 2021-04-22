4895856e0e4e33bfbbda8a0e9daedb2f710488ff
Yun Chung <yun.chung@mac.com>
2021-04-22 12:00:43 +0900

    Added route for admin access

--- React-Apps/mern-authentication/routes/User.js
+++ React-Apps/mern-authentication/routes/User.js
@@ -77,4 +77,12 @@ userRouter.get('/todos', passport.authenticate('jwt', { session: false }), (req,
     });
 });
 
+userRouter.get('/admin', passport.authenticate('jwt', { session: false }), (req, res) => {
+    if (req.user.role === "admin") {
+        res.status(200).json({ message: { msgBody: "You are an admin", msgError: false } });
+    } else {
+        res.status(403).json({ message: { msgBody: "You are NOT an admin", msgError: true } });
+    }
+});
+
 module.exports = userRouter;
