ad2900d18ed87823f4b86787faa987f5d98c626b
Yun Chung <yun.chung@mac.com>
2021-04-23 09:54:55 +0900

    Added authenticated route for React persistence of authentication

--- React-Apps/mern-authentication/routes/User.js
+++ React-Apps/mern-authentication/routes/User.js
@@ -85,4 +85,10 @@ userRouter.get('/admin', passport.authenticate('jwt', { session: false }), (req,
     }
 });
 
+// Maintains login persistence for React...
+userRouter.get('/authenticated', passport.authenticate('jwt', { session: false }), (req, res) => {
+    const { username, role } = req.user;
+    res.status(200).json({ isAuthenticated: true, user: { username, role } });
+});
+
 module.exports = userRouter;
