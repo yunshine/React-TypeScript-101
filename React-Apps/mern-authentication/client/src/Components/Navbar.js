37a5093d6c4113f2611c5343d1ae3823a0d30be6
Yun Chung <yun.chung@mac.com>
2021-04-30 11:53:46 +0900

    Created navbar for unauthenticated user

--- React-Apps/mern-authentication/client/src/Components/Navbar.js
+++ React-Apps/mern-authentication/client/src/Components/Navbar.js
@@ -4,8 +4,35 @@ import AuthService from '../Services/AuthService';
 import { AuthContext } from '../Context/AuthContext';
 
 const Navbar = (props) => {
+    const { isAuthenticated, user, setIsAuthenticated, setUser } = useContext(AuthContext);
+
+    const unauthenticatedNavbar = () => {
+        return (
+            <>
+                <Link to="/">
+                    <li className="nav-item nav-link">Home</li>
+                </Link>
+                <Link to="/login">
+                    <li className="nav-item nav-link">Login</li>
+                </Link>
+                <Link to="/register">
+                    <li className="nav-item nav-link">Register</li>
+                </Link>
+            </>
+        );
+    }
+
     return (
-        
+        <nav class="navbar navbar-expand-lg navbar-light bg-light">
+            <Link to="/">
+                <div className="navbar-brand">Home</div>
+            </Link>
+            <div class="collapse navbar-collapse" id="navbarText">
+                <ul class="navbar-nav mr-auto">
+                    {!isAuthenticated ? unauthenticatedNavbar() : authenticatedNavbar()}
+                </ul>
+            </div>
+        </nav >
     );
 }
 
