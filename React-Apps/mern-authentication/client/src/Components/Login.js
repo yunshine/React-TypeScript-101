86a9793b4139ded7c5fbe9e33adb87c89f11bf65
Yun Chung <yun.chung@mac.com>
2021-05-06 14:01:13 +0900

    Completed login form

--- React-Apps/mern-authentication/client/src/Components/Login.js
+++ React-Apps/mern-authentication/client/src/Components/Login.js
@@ -8,8 +8,36 @@ const Login = (props) => {
     const [message, setMessage] = useState(null);
     const authContext = useContext(AuthContext);
 
+    function onSubmit() {
+        console.log("form submitted...");
+    }
+
+    function onChange() {
+        console.log("Something is changing...");
+    }
+
     return (
-        
+        <div>
+            <form onSubmit={onSubmit}>
+                <h3>Please Sign In</h3>
+                <label htmlFor="username" className="sr-only">Username: </label>
+                <input type="text"
+                    name="username"
+                    onChange={onChange}
+                    className="form-control"
+                    placeholder="Enter Username" />
+                <label htmlFor="password" className="sr-only">Password: </label>
+                <input type="text"
+                    name="password"
+                    onChange={onChange}
+                    className="form-control"
+                    placeholder="Enter Password" />
+                <button className="btn btn-lg btn-primary btn-block"
+                    type="submit">Log In</button>
+            </form>
+
+            {message ? <Message message={message} /> : null}
+        </div>
     );
 }
 
