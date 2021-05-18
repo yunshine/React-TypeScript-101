bafcdbb144c8697d9d0498735e22031048d5078c
Yun Chung <yun.chung@mac.com>
2021-05-18 11:21:17 +0900

    Started todos component

---
+++ React-Apps/mern-authentication/client/src/Components/Todos.js
@@ -0,0 +1,17 @@
+import React, { useState, useContext, useEffect } from 'react';
+import TodoItem from './TodoItem';
+import TodoService from '../Services/TodoService';
+import { AuthContext } from '../Context/AuthContext';
+
+const Todos = (props) => {
+    const [todo, setTodo] = useState({ name: "" });
+    const [todos, setTodos] = useState([]);
+    const [message, setMessage] = useState(null);
+    const AuthContext = useContext(AuthContext);
+
+    return (
+
+    );
+}
+
+export default Todos;
