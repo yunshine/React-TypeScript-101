f6854e2716e36685202842788b55daf98c18e7e2
Yun Chung <yun.chung@mac.com>
2021-05-27 12:20:48 +0900

    completed todo component render

--- React-Apps/mern-authentication/client/src/Components/Todos.js
+++ React-Apps/mern-authentication/client/src/Components/Todos.js
@@ -16,7 +16,27 @@ const Todos = (props) => {
     }, []);
 
     return (
-
+        <div>
+            <ul className="list-group">
+                {
+                    todos.map(todo => {
+                        return <TodoItem key={todo_id} todo={todo} />
+                    })
+                }
+            </ul>
+            <br />
+            <form onSubmit={onSubmit}>
+                <label htmlFor="todo">Enter Todo Item</label>
+                <input
+                    type="text"
+                    className="form-control"
+                    name="todo"
+                    placeholder="Please Enter Todo Item"
+                    value={todo.name}
+                    onChange={ } />
+                <button type="submit" className="btn btn-lg btn-primary btn-block">Submit</button>
+            </form>
+        </div>
     );
 }
 
