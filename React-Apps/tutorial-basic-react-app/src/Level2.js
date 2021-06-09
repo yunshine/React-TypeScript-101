80a7fd9505ab10711ea1933ae20a68dad90ac036
Yun Chung <yun.chung@mac.com>
2021-06-09 12:24:23 +0900

    Completed route and template for level 2

--- React-Apps/tutorial-basic-react-app/src/Level2.js
+++ React-Apps/tutorial-basic-react-app/src/Level2.js
@@ -1,6 +1,37 @@
+import { useState } from 'react';
+
 const Level2 = () => {
+    const [count, setCount] = useState(0);
+    // let number = 10;
+
+    const countUp = () => {
+        setCount(count + 1);
+    }
+
+    const countDown = () => {
+        setCount(count - 1);
+    }
+
+    const countReset = () => {
+        setCount(0);
+    }
+
+    // const numberUp = () => {
+    //     number += 1;
+    // }
+
     return (
-        <div>aldkfjasdlkfj</div>
+        <div>
+            <h2>{count}</h2>
+            <button onClick={countUp}>Up</button>
+            <button onClick={countDown}>Down</button>
+            <button onClick={countReset}>Reset</button>
+
+            {/* <div>
+                <h1>{number}</h1>
+                <button onClick={numberUp}>Number Up</button>
+            </div> */}
+        </div>
     );
 }
 
