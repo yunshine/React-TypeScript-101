4e86246ecd6a71054658c41a677389c3083b5deb
Yun Chung <yun.chung@mac.com>
2021-06-21 10:49:45 +0900

    Created privateroutes component

---
+++ React-Apps/mern-authentication/client/src/HigherOrderComponents/PrivateRoute.js
@@ -0,0 +1,8 @@
+import { useContext } from "react";
+import { Route, Redirect } from "react-router";
+import { AuthContext } from "../Context/AuthContext";
+// This component protects the routes that you should be logged in for...
+
+const PrivateRoute = (props) => {
+
+}
