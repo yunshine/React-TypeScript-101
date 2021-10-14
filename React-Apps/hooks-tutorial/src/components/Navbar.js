77b95869109d8f9b3b0cf8d40b671f551c69a271
Yun Chung <yun.chung@mac.com>
2021-10-14 13:21:47 +0900

    removed unnecessary react files and created navbar component

---
+++ React-Apps/hooks-tutorial/src/components/Navbar.js
@@ -0,0 +1,14 @@
+const Navbar = () => {
+    return (
+        <nav>
+            <h1>Hooks Tutorial App</h1>
+            <ul>
+                <li>Home</li>
+                <li>About</li>
+                <li>Contact</li>
+            </ul>
+        </nav>
+    );
+}
+
+export default Navbar;
