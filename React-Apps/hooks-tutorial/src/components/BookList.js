e107d27caca3c2933e277913be7ae34b8b98df58
Yun Chung <yun.chung@mac.com>
2021-10-14 13:28:01 +0900

    added booklist compoonent

---
+++ React-Apps/hooks-tutorial/src/components/BookList.js
@@ -0,0 +1,13 @@
+const BookList = () => {
+    return (
+        <div>
+            <ul>
+                <li>The Bible</li>
+                <li>The Alchemist</li>
+                <li>A Game of Thrones</li>
+            </ul>
+        </div>
+    );
+}
+
+export default BookList;
