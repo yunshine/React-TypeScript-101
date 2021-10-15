17cd1d4e47813f5de31c43be2be5a44232b3d937
Yun Chung <yun.chung@mac.com>
2021-10-15 18:37:12 +0900

    used context.consumer in navbar component

--- React-Apps/hooks-tutorial/src/components/Navbar.js
+++ React-Apps/hooks-tutorial/src/components/Navbar.js
@@ -1,20 +1,31 @@
-import { useContext } from 'react';
+// An older, but different, way of consuming context vs. what is in BookList.js...
+
+// import { useContext } from 'react'; => different syntax than BookList.js
+import { Component } from 'react';
 import { ThemeContext } from '../contexts/ThemeContext';
 
+// class Navbar extends Component {
 const Navbar = () => {
-    const { isLightTheme, light, dark } = useContext(ThemeContext);
-    const theme = isLightTheme ? light : dark;
-
+    // render() {
     return (
-        <nav style={{ background: theme.ui, color: theme.syntax }}>
-            <h1>Hooks Tutorial App</h1>
-            <ul>
-                <li>Home</li>
-                <li>About</li>
-                <li>Contact</li>
-            </ul>
-        </nav>
+        // context consumed in this component using ThemeContext.Consumer...
+        <ThemeContext.Consumer>{(context) => {
+            // const { isLightTheme, light, dark } = useContext(ThemeContext); => different syntax than BookList.js
+            const { isLightTheme, light, dark } = context;
+            const theme = isLightTheme ? light : dark;
+            return (
+                <nav style={{ background: theme.ui, color: theme.syntax }}>
+                    <h1>Hooks Tutorial App</h1>
+                    <ul>
+                        <li>Home</li>
+                        <li>About</li>
+                        <li>Contact</li>
+                    </ul>
+                </nav>
+            )
+        }}</ThemeContext.Consumer>
     );
+    // };
 }
 
 export default Navbar;
