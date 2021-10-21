39250722d5b43eb817a46aa209a039eb466148c0
Yun Chung <yun.chung@mac.com>
2021-10-21 17:24:32 +0900

    added ability to add a new hard-coded song to the songlist

--- React-Apps/hooks-tutorial/src/components/SongList.js
+++ React-Apps/hooks-tutorial/src/components/SongList.js
@@ -3,6 +3,10 @@ import { useState } from 'react';
 const SongList = () => {
     const [songs, setSongs] = useState([{ title: "One Life", id: 1 }, { title: "Despacito", id: 2 }, { title: "Snappy", id: 3 }]);
 
+    const addSong = () => {
+        setSongs([...songs, { title: "testing", id: songs.length + 1 }]);
+    }
+
     return (
         <div className="SongList">
             <ul>
@@ -10,6 +14,7 @@ const SongList = () => {
                     return (<li key={song.id}>{song.title}</li>)
                 })}
             </ul>
+            <button onClick={addSong}>Add A New Song</button>
         </div>
     );
 }
