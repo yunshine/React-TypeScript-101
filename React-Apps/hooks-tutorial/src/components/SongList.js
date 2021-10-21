b1ef758138f3be274a751dca5f86ec4195349c07
Yun Chung <yun.chung@mac.com>
2021-10-21 17:19:18 +0900

    displays songs dynamically using useState

--- React-Apps/hooks-tutorial/src/components/SongList.js
+++ React-Apps/hooks-tutorial/src/components/SongList.js
@@ -1,10 +1,14 @@
+import { useState } from 'react';
+
 const SongList = () => {
+    const [songs, setSongs] = useState([{ title: "One Life", id: 1 }, { title: "Despacito", id: 2 }, { title: "Snappy", id: 3 }]);
+
     return (
         <div className="SongList">
             <ul>
-                <li>One Life</li>
-                <li>Despacito</li>
-                <li>Snappy</li>
+                {songs.map(song => {
+                    return (<li key={song.id}>{song.title}</li>)
+                })}
             </ul>
         </div>
     );
