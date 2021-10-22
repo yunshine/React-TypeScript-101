b5af9a3a4f224aa28e2cad26f62d4b1a7622910d
Yun Chung <yun.chung@mac.com>
2021-10-22 16:46:41 +0900

    NewSongForm functionality complete

--- React-Apps/hooks-tutorial/src/components/NewSongForm.js
+++ React-Apps/hooks-tutorial/src/components/NewSongForm.js
@@ -1,8 +1,20 @@
 import { useState } from 'react';
 
-const NewSongForm = () => {
-    return (
+const NewSongForm = (props) => {
+    const [songName, setSongName] = useState("");
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        console.log(songName);
+        props.addSong(songName);
+        setSongName("");
+    }
 
+    return (
+        <form onSubmit={handleSubmit}>
+            <label for="song-name">Song Name:</label>
+            <input type="text" value={songName} id="song-name" onChange={(e) => setSongName(e.target.value)} required />
+            <input type="submit" value="Add Song" />
+        </form>
     );
 }
 
--- React-Apps/hooks-tutorial/src/components/SongList.js
+++ React-Apps/hooks-tutorial/src/components/SongList.js
@@ -1,10 +1,11 @@
 import { useState } from 'react';
+import NewSongForm from './NewSongForm';
 
 const SongList = () => {
     const [songs, setSongs] = useState([{ title: "One Life", id: 1 }, { title: "Despacito", id: 2 }, { title: "Snappy", id: 3 }]);
 
-    const addSong = () => {
-        setSongs([...songs, { title: "testing", id: songs.length + 1 }]);
+    const addSong = (title) => {
+        setSongs([...songs, { title: title, id: songs.length + 1 }]);
     }
 
     return (
@@ -14,7 +15,8 @@ const SongList = () => {
                     return (<li key={song.id}>{song.title}</li>)
                 })}
             </ul>
-            <button onClick={addSong}>Add A New Song</button>
+            {/* <button onClick={addSong}>Add A New Song</button> */}
+            <NewSongForm addSong={addSong} />
         </div>
     );
 }
