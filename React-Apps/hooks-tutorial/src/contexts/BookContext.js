f43134733c9afc65b348772bc45ab3094e722d01
Yun Chung <yun.chung@mac.com>
2021-10-24 19:40:56 +0900

    booklist to useState hook

--- React-Apps/hooks-tutorial/src/contexts/BookContext.js
+++ React-Apps/hooks-tutorial/src/contexts/BookContext.js
@@ -1,9 +1,11 @@
 import { useState, useContext } from 'react';
 
-const BookContext = () => {
-    return (
+export const BookContext = createContext();
 
-    );
+const BookContextProvider = () => {
+    const [books, setBooks] = useState([{ title: "A Game of Thrones", author: "George R. R. Martin", id: 1 }, { title: "The Alchemist", author: "Paulo Coelho", id: 2 }, { title: "The Bible", author: "God", id: 3 }]);
+
+
 }
 
-export default BookContext;
+export default BookContextProvider;
