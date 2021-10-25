7f419ed065ac4e01f4b7b3dfd381eedb696e97c1
Yun Chung <yun.chung@mac.com>
2021-10-25 11:37:54 +0900

    BookContext jsx

--- React-Apps/hooks-tutorial/src/contexts/BookContext.js
+++ React-Apps/hooks-tutorial/src/contexts/BookContext.js
@@ -2,7 +2,7 @@ import { useState, useContext } from 'react';
 
 export const BookContext = createContext();
 
-const BookContextProvider = () => {
+const BookContextProvider = ({ children }) => {
     const [books, setBooks] = useState([{ title: "A Game of Thrones", author: "George R. R. Martin", id: 1 }, { title: "The Alchemist", author: "Paulo Coelho", id: 2 }, { title: "The Bible", author: "God", id: 3 }]);
 
     const addBook = (title, author) => {
@@ -12,6 +12,12 @@ const BookContextProvider = () => {
     const removeBook = (id) => {
         setBooks(books.filter(book => book.id !== id));
     }
+
+    return (
+        <BookContext.Provider value={{ books, addBook, removeBook }}>
+            {children}
+        </BookContext.Provider>
+    );
 
 }
 
