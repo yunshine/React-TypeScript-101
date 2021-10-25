57e15ceac61f44b9f8edbe061dd531f03a3d8697
Yun Chung <yun.chung@mac.com>
2021-10-25 11:30:40 +0900

    created addBooks function

--- React-Apps/hooks-tutorial/src/contexts/BookContext.js
+++ React-Apps/hooks-tutorial/src/contexts/BookContext.js
@@ -5,6 +5,10 @@ export const BookContext = createContext();
 const BookContextProvider = () => {
     const [books, setBooks] = useState([{ title: "A Game of Thrones", author: "George R. R. Martin", id: 1 }, { title: "The Alchemist", author: "Paulo Coelho", id: 2 }, { title: "The Bible", author: "God", id: 3 }]);
 
+    const addBook = (title, author) => {
+        setBooks([...books, { title: title, author: author, id: books.length + 1 }]);
+    }
+
 
 }
 
