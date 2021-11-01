510cbf1094a472ddca1d2ae8f661d12c9792f6c9
Yun Chung <yun.chung@mac.com>
2021-11-01 16:19:51 +0900

    created bookReducer file

---
+++ React-Apps/hooks-tutorial/src/reducers/bookReducer.js
@@ -0,0 +1,10 @@
+export const bookReducer = (state, action) => {
+    switch (action.type) {
+        case "ADD_BOOK":
+            return [...state, { title: action.book.title, author: action.bok.author, id: state.length + 1 }];
+        case "REMOVE_BOOK":
+            return state.filter(book => book.title !== action.title);
+        default:
+            return state;
+    }
+}
