e79abe3f7603851883528d6fd1084537f802c903
Yun Chung <yun.chung@mac.com>
2021-11-01 16:24:58 +0900

    BookContext file now uses bookReducer

--- React-Apps/hooks-tutorial/src/contexts/BookContext.js
+++ React-Apps/hooks-tutorial/src/contexts/BookContext.js
@@ -1,20 +1,13 @@
-import { useState, createContext } from 'react';
+import { createContext, useReducer } from 'react';
+import { bookReducer } from '../reducers/bookReducer';
 
 export const BookContext = createContext();
 
 const BookContextProvider = ({ children }) => {
-    const [books, setBooks] = useState([{ title: "A Game of Thrones", author: "George R. R. Martin", id: 1 }, { title: "The Alchemist", author: "Paulo Coelho", id: 2 }, { title: "The Bible", author: "God", id: 3 }]);
-
-    const addBook = (title, author) => {
-        setBooks([...books, { title: title, author: author, id: books.length + 1 }]);
-    }
-
-    const removeBook = (title) => {
-        setBooks(books.filter(book => book.title !== title));
-    }
+    const [books, dispatch] = useReducer(bookReducer, []);
 
     return (
-        <BookContext.Provider value={{ books, addBook, removeBook }}>
+        <BookContext.Provider value={{ books, dispatch }}>
             {children}
         </BookContext.Provider>
     );
