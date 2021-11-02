9f2237e0768c4f6051b3d727b2a59af1ebf1cac2
Yun Chung <yun.chung@mac.com>
2021-11-02 16:19:45 +0900

    added notes and tests for testing floating point numbers

--- React-Apps/Jest 101/sum.test.js
+++ React-Apps/Jest 101/sum.test.js
@@ -56,3 +56,11 @@ test('two plus two', () => {
     expect(value).toBe(4);
     expect(value).toEqual(4);
 });
+
+
+// For floating point equality, use toBeCloseTo instead of toEqual, because you don't want a test to depend on a tiny rounding error.
+test('adding floating point numbers', () => {
+    const value = 0.1 + 0.2;
+    //expect(value).toBe(0.3);  This won't work because of rounding error
+    expect(value).toBeCloseTo(0.3);  // This works.
+});
