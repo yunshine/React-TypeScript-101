ac2f246bbeef85070767803ba8efe852978d7148
Yun Chung <yun.chung@mac.com>
2021-11-02 16:22:32 +0900

    added notes and tests for testing strings

--- React-Apps/Jest 101/sum.test.js
+++ React-Apps/Jest 101/sum.test.js
@@ -64,3 +64,13 @@ test('adding floating point numbers', () => {
     //expect(value).toBe(0.3);  This won't work because of rounding error
     expect(value).toBeCloseTo(0.3);  // This works.
 });
+
+
+// You can check strings against regular expressions with toMatch:
+test('there is no I in team', () => {
+    expect('team').not.toMatch(/I/);
+});
+
+test('but there is a "stop" in Christoph', () => {
+    expect('Christoph').toMatch(/stop/);
+});
