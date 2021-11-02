155ecef0838f8f7490a07f7e0584d46e75bd60f9
Yun Chung <yun.chung@mac.com>
2021-11-02 13:54:10 +0900

    added notes for using the keyword not and strict equality in Jest

--- React-Apps/Jest 101/sum.test.js
+++ React-Apps/Jest 101/sum.test.js
@@ -1,9 +1,21 @@
 const sum = require('./sum');
+// const subtract = require('./subtract');  -  How to import from different files...
 
+// "expect" and "to be"
 test('adds 1 + 2 to equal 3', () => {
     expect(sum(1, 2)).toBe(3);
 });
 
-test('adds 30 + 9 to equal 39', () => {
-    expect(sum(30, 9)).toBe(39);
+// "expect" and "to be" with "not"
+test('adds 1 + 2 to NOT equal 0', () => {
+    expect(sum(1, 2)).not.toBe(0);
 });
+
+// NOTE: Jest checks for strict equality, so the test below will fail...
+test('adds "1" + "1" to equal 11', () => {
+    expect(sum("1", "1")).toBe(11);
+});
+
+// test('subtracts 1 from 40 to equal 39', () => {
+//     expect(subtract(40, 1)).toBe(39);
+// });
