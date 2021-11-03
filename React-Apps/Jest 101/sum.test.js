a6d6f61f59f041de0a575b86ca3e3da4d15a1b7e
Yun Chung <yun.chung@mac.com>
2021-11-03 09:54:04 +0900

    added notes for throwing errors

--- React-Apps/Jest 101/sum.test.js
+++ React-Apps/Jest 101/sum.test.js
@@ -90,3 +90,19 @@ test('the shopping list has milk on it', () => {
     expect(['salmon', 'ginger ale', 'pesto']).toContain('salmon');
     expect(new Set(shoppingList)).toContain('milk');
 });
+
+
+// If you want to test whether a particular function throws an error when it's called, use toThrow.
+// Note: the function that throws an exception needs to be invoked within a wrapping function otherwise the toThrow assertion will fail.
+function compileAndroidCode() {
+    throw new Error('you are using the wrong JDK');
+}
+
+test('compiling android goes as expected', () => {
+    expect(() => compileAndroidCode()).toThrow();
+    expect(() => compileAndroidCode()).toThrow(Error);
+
+    // You can also use the exact error message or a regexp
+    expect(() => compileAndroidCode()).toThrow('you are using the wrong JDK');
+    expect(() => compileAndroidCode()).toThrow(/JDK/);
+});
