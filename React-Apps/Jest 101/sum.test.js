fb70c139ab6f2df99b064d8e5610e85e3e3ff718
Yun Chung <yun.chung@mac.com>
2021-11-08 17:25:28 +0900

    added notes for testing async code that resolves

--- React-Apps/Jest 101/sum.test.js
+++ React-Apps/Jest 101/sum.test.js
@@ -160,3 +160,9 @@ test('the fetch fails with an error', () => {
     expect.assertions(1);
     return fetchDataWithPromise().catch(e => expect(e).toMatch('error'));
 });
+
+// You can also use the .resolves matcher in your expect statement, and Jest will wait for that promise to resolve. If the promise is rejected, the test will automatically fail.
+test('the data is peanut butter', () => {
+    return expect(fetchData()).resolves.toBe('peanut butter');
+});
+// Be sure to return the assertion—if you omit this return statement, your test will complete before the promise returned from fetchData is resolved and then() has a chance to execute the callback.
