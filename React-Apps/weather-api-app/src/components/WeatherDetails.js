fd951ebf5767583c0ae22bbe6a92cd5972dd4f7c
Yun Chung <yun.chung@mac.com>
2021-11-18 13:56:11 +0900

    able to successfully fetch weather data from api

--- React-Apps/weather-api-app/src/components/WeatherDetails.js
+++ React-Apps/weather-api-app/src/components/WeatherDetails.js
@@ -5,14 +5,10 @@ const WeatherDetails = () => {
     const { city } = useContext(CityContext);
     const [isLoading, setIsLoading] = useState(false);
 
-    // Async & Await
-    // ==========================================================================
-    // first, create  a function that contains all of our async code, the code that goes out to get data from somewhere. Because we've used the keyword 'async', it is non-blocking...
-
-    // whenever we use the async key word, the function will return a PROMISE...
-    const getTodosWithAsyncAwait = async () => {
+    // Because we've used the keyword 'async', it is non-blocking. And, whenever we use the async key word, the function will return a PROMISE...
+    const getWeatherData = async () => {
         // once the fetch Promise below is resolved, JavaScript will then assign the data as the value to 'response'... this data will be a response object...
-        const response = await fetch('json/todos.json');  // using the keyword "await" will make it so that this fetch returns a promise...
+        const response = await fetch(`http://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${process.env.REACT_APP_API_KEY}`);  // using the keyword "await" will make it so that this fetch returns a promise...
         console.log("The 'response' variable from getTodosWithAsyncAwait: ", response);
 
         if (response.status !== 200) {
@@ -21,19 +17,16 @@ const WeatherDetails = () => {
         }
 
         const data = await response.json();
-        console.log("The 'data' variable from getTodosWithAsyncAwait: ", data);
-
         return data;
     };
 
     useEffect(() => {
-        console.log("use effecting...");
         if (city.length) {
             setIsLoading(true);
             setTimeout(() => {
-                getTodosWithAsyncAwait()
-                    .then(data => console.log("Resolved data from getTodosWithAsyncAwait: ", data))
-                    .catch(err => console.log("Err from getTodosWithAsyncAwait: ", err.message));
+                getWeatherData()
+                    .then(data => console.log("Resolved data from getWeatherData: ", data))
+                    .catch(err => console.log("Err from getWeatherData: ", err.message));
                 setIsLoading(false)
             }, 3000);
         }
