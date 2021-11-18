559ffb68b30727db54b76d20b43be113c4d4cefc
Yun Chung <yun.chung@mac.com>
2021-11-18 14:08:04 +0900

    added timeConversion function and rendered weather data that involves time

--- React-Apps/weather-api-app/src/components/WeatherDetails.js
+++ React-Apps/weather-api-app/src/components/WeatherDetails.js
@@ -36,10 +36,33 @@ const WeatherDetails = () => {
         }
     }, [city]);
 
+    function timeConversion(timestamp) {
+        let unix_timestamp = timestamp;
+        // Create a new JavaScript Date object based on the timestamp
+        // multiplied by 1000 so that the argument is in milliseconds, not seconds.
+        let date = new Date(unix_timestamp * 1000);
+        // Hours part from the timestamp
+        let hours = date.getHours();
+        // Minutes part from the timestamp
+        let minutes = "0" + date.getMinutes();
+        // Seconds part from the timestamp
+        // let seconds = "0" + date.getSeconds();
+        let suffix = "am";
+        // Dynamic AM/PM
+        if (hours === 12) suffix = "pm";
+        if (hours > 12) {
+            hours -= 12;
+            suffix = "pm";
+        }
+        // Will display time in 10:30:23 format
+        // let formattedTime = hours + ':' + minutes.substr(-2) + ':' + seconds.substr(-2);
+        let formattedTime = hours + ':' + minutes.substr(-2) + suffix;
+        return formattedTime;
+    };
+
     return (
         <div className="WeatherDetails">
             {isLoading && "Loading Weather Data..."}
-            {!isLoading && city}
             {!isLoading &&
                 <div className="WeatherData">
                     <h2>Current Temperature in {weatherData.name}: {(weatherData.main.temp - 273.15).toFixed(1)} C</h2>
@@ -47,9 +70,9 @@ const WeatherDetails = () => {
                     <img src={`http://openweathermap.org/img/w/${weatherData.weather[0].icon}.png`} alt="weather" />
                     <h2>{weatherData.weather[0].description}</h2>
                     <h2>Wind: {(weatherData.wind.speed).toFixed(0)} m/s</h2>
-                    {/* <h2>Current Time: {timeConversion(weatherData.dt)}</h2> */}
-                    {/* <h2>Sunrise: {timeConversion(weatherData.sys.sunrise)}</h2> */}
-                    {/* <h2>Sunset: {timeConversion(weatherData.sys.sunset)}</h2> */}
+                    <h2>Current Time: {timeConversion(weatherData.dt)}</h2>
+                    <h2>Sunrise: {timeConversion(weatherData.sys.sunrise)}</h2>
+                    <h2>Sunset: {timeConversion(weatherData.sys.sunset)}</h2>
                 </div>
             }
         </div>
