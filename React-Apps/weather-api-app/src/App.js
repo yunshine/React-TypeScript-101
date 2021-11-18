fd89f5d92dcf02ec21dcee8623539c95d25d796d
Yun Chung <yun.chung@mac.com>
2021-11-18 14:10:00 +0900

    removed pseudo code from app.js file

--- React-Apps/weather-api-app/src/App.js
+++ React-Apps/weather-api-app/src/App.js
@@ -15,21 +15,3 @@ function App() {
 }
 
 export default App;
-
-/* City Context
-    keeps track of city and makes the city available to components
-
-
-City Reducer
-    allows the city context's state to be updated
-
-City Form Component
-    sets the city and sends it to the dispatch
-
-Weather Details
-    fetches data from API; sets isLoading state; displays the weather
-information
-
-Weather App Component
-    holds form, weather details, and "loading" render
-*/
